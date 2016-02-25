9f60cfcb97377c8f569d5157b7c876bfd42b267e
Vlad Fridkin <vladislavfridkin@gmail.com>
2016-02-25 11:09:15 +1100

    Updated shiny UI with descriptions of plots

--- AusPop/ui.R
+++ AusPop/ui.R
@@ -7,15 +7,15 @@ shinyUI(fluidPage(
     href="http://stat.abs.gov.au/Index.aspx?DatasetCode=POP_PROJ_2011"
     )
   ),
-  p("Here you can visualise the population projections from ABS (Australian Bureau of Statistics),
+  p("Visualise population projections from ABS (Australian Bureau of Statistics),
     alter assumptions, choose year ranges and see the effects on predicted populations.  
     It's common knowledge in Australia that the proportion of elderly people is increasing.
-    This effect is easily seen by viewing the", em('change'), "plots.
-    However there are other interesting effects to discover too, such as how migration assumptions 
-    have a profound effect on the demographics of younger age groups.
+    Assuming", em("zero net migration"), "this is clearly visible in the", em('Changes'), "plots below.
+    Other interesting effects to discover include how migration assumptions 
+    have a profound effect on younger age groups.
     "),
-  p("One of the uses of this visualisation is for developing business strategy for industries that depend on 
-    demographics.  For example, with default", em('medium'), "assumptions, one can see in the", em('Percentage change'), "chart that the the 
+  p("Uses of this visualisation include informing business strategy that targets age groups. 
+    With", em('medium'), "assumptions, the", em('Percentage change'), "chart indicates that the 
     20 year old group changes about 5% over 10 years compared to a growth of over 20% for the 35 year olds."),
   p(strong("Instructions:"), "Choose the year range with the slider.  By default, only values for the first and last year are shown.
     Change this by checking", em("Plot all years."), "Alter assumptions using the drop down boxes.  You can focus on genders seperately
@@ -56,10 +56,28 @@ shinyUI(fluidPage(
     
     mainPanel(
       tabsetPanel(type = "tabs", 
-                  tabPanel("Totals by age", plotOutput("totals")),
-                  tabPanel("Change", plotOutput("absChange")), 
-                  tabPanel("Percentage change", plotOutput("relChange"))
+                  tabPanel("Totals", plotOutput("totals")),
+                  tabPanel("Changes", plotOutput("absChange")), 
+                  tabPanel("Percentage changes", plotOutput("relChange"))
       )
     )
-  )
+  ),
+  
+  p(strong("Totals:"), "each dot represents the number of people at the same age in a given year.  
+    When multiple years are shown, these dots trace out lines.  These lines represent the same group of people
+    getting older through time.  With zero migration, the younger groups move horizontally as expected, i.e.
+    new people don't appear out of thin air.  The older groups slope downwards as the death rate increases.
+    When it is above the slope of differences between successive age groups, this indicates an 
+    increasing life expectancy."),
+  p(strong("Changes:"), "each dot represents the change in the number of people at the same age in a given year
+    from the base year.  The base year is the first year chosen in the year range slider.  
+    When multiple years are shown, with some effort, one can make out fluctuations.
+    With zero migration, the bias towards an increase in the aging population is clearly visible."),
+  p(strong("Percentage changes:"), "is the percentage change relative to the base year.  
+    Small changes may show large effects if they are large relative to the base year.")
+  
+  
+
+  
+  
 ))
